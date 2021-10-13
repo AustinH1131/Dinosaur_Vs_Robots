111e1e63c5a5aebad241c8a3d355aeba8ce16891
austin h <athleticatxbox2@gmail.com>
2021-10-13 09:41:37 -0700

    All classes completed

---
+++ Battlefield.py
@@ -0,0 +1,46 @@
+from Fleet import Fleet
+from Herd import Herd
+
+
+class Battlefield:
+    def __init__(self):
+        self.fleet=Fleet()
+        self.herd=Herd()
+    
+    def run_game(self):
+        #Void
+        #Toggle game on and off
+        pass
+
+    def display_welcome(self):
+        #Void
+        #Welcome a user to the game with a print
+        pass
+
+    def battle(self):
+        #Starts the battle between dinos and robots
+        #Void
+        pass
+
+    def dino_turn(self,Dinasaur):
+        #Shifts the field of play into the dinos turn.
+        #Void
+        pass
+
+    def robo_turn(self,Robot):
+        #Shifts field of play into the robots turn.
+        #Void
+        pass
+
+    def show_dino_opponet_options(self):
+        #Void
+        pass
+
+    def show_robo_opponet_options(self):
+        #Void
+        pass
+
+    def display_winners(self):
+        #Prints the winner of the battle to the screen/console
+        #Void
+        pass
